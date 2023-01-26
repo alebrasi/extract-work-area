e9aa2091d100805c2f7f6605c3d8b578a8f07657
alebrasi <alessandro.brasini1@gmail.com>
2023-01-26 17:06:58 +0100

    Lateral white band removal

--- crop.py
+++ crop.py
@@ -4,40 +4,89 @@ import matplotlib
 import matplotlib.pyplot as plt
 import math
 
-def create_gaborfilter():
-    # This function is designed to produce a set of GaborFilters 
-    # an even distribution of theta values equally distributed amongst pi rad / 180 degree
+# TODO: Fare una funzione più corta
+# https://gist.github.com/flashlib/e8261539915426866ae910d55a3f9959
+def order_points(pts):
+    # sort the points based on their x-coordinates
+    xSorted = pts[np.argsort(pts[:, 0]), :]
 
-    filters = []
-    num_filters = 16
-    ksize = 35  # The local area to evaluate
-    sigma = 2.0  # Larger Values produce more edges
-    lambd = 20.0
-    gamma = 0.5
-    psi = 0  # Offset value - lower generates cleaner results
-    for theta in np.arange(0, np.pi, np.pi / num_filters):  # Theta is the orientation for edge detection
-        kern = cv.getGaborKernel((ksize, ksize), sigma, theta, lambd, gamma, psi, ktype=cv.CV_64F)
-        kern /= 1.0 * kern.sum()  # Brightness normalization
-        filters.append(kern)
-    return filters
+    # grab the left-most and right-most points from the sorted
+    # x-roodinate points
+    leftMost = xSorted[:2, :]
+    rightMost = xSorted[2:, :]
 
+    # now, sort the left-most coordinates according to their
+    # y-coordinates so we can grab the top-left and bottom-left
+    # points, respectively
+    leftMost = leftMost[np.argsort(leftMost[:, 1]), :]
+    (tl, bl) = leftMost
 
-def apply_filter(img, filters):
-# This general function is designed to apply filters to our image
-    # First create a numpy array the same size as our input image
-    newimage = np.zeros_like(img)
+    # if use Euclidean distance, it will run in error when the object
+    # is trapezoid. So we should use the same simple y-coordinates order method.
 
-    # Starting with a blank image, we loop through the images and apply our Gabor Filter
-    # On each iteration, we take the highest value (super impose), until we have the max value across all filters
-    # The final image is returned
-    depth = -1 # remain depth same as original image
+    # now, sort the right-most coordinates according to their
+    # y-coordinates so we can grab the top-right and bottom-right
+    # points, respectively
+    rightMost = rightMost[np.argsort(rightMost[:, 1]), :]
+    (tr, br) = rightMost
 
-    for kern in filters:  # Loop through the kernels in our GaborFilter
-        image_filter = cv.filter2D(img, depth, kern)  #Apply filter to image
+    # return the coordinates in top-left, top-right,
+    # bottom-right, and bottom-left order
+    return [tl, tr, br, bl]
 
-        # Using Numpy.maximum to compare our filter and cumulative image, taking the higher value (max)
-        np.maximum(newimage, image_filter, newimage)
-    return newimage
+def remove_lateral_white_bands(img):
+    band_range = [(19, 15, 180), (24, 65, 230)]
+    #band_range_clahe = [(25, 18, 170), (35, 90, 235)]
+
+    img = cv.GaussianBlur(img, (31, 31), 60)
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    # Remove noise
+    hsv = cv.pyrDown(hsv)
+    hsv = cv.pyrUp(hsv)
+
+    thr = cv.inRange(hsv, band_range[0], band_range[1])
+    ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (50, 50))
+    #thr = cv.morphologyEx(thr, cv.MORPH_CLOSE, ker)
+    thr = cv.dilate(thr, ker)
+
+    #contours = cv.cvtColor(cropped, cv.COLOR_GRAY2BGR)
+    
+    rects = []
+
+    cnts, hierarchy = cv.findContours(thr, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    for i, cnt in enumerate(hierarchy[0]):
+
+        # Contour approximation
+        perimeter = cv.arcLength(cnts[i], True)
+        epsilon = 0.02 * perimeter
+        approx = cv.approxPolyDP(cnts[i], epsilon, True)
+
+        # Discard non squares/rectangles and non convex        
+        if len(approx) != 4 and not cv.isContourConvex(cnts[i]):
+            continue
+        
+        points = [ point[0] for point in approx ]
+        points = order_points(np.array(points))
+        
+        top_left, top_right, bottom_right, bottom_left = points[:4]
+
+        norm = lambda pt1, pt2: math.sqrt(((pt1[0]-pt2[0])**2) + ((pt1[1]-pt2[1])**2))
+
+        w, h = norm(top_left, top_right), norm(top_left, bottom_left)
+        if h < 1750 or w > h:
+            continue
+  
+        rects.append(cv.minAreaRect(approx))
+
+    h_i, w_i = img.shape[:2]
+    mask = np.ones((h_i, w_i), dtype='uint8')
+    c_x = h_i//2
+    for rect in rects:
+        rect = np.int0(cv.boxPoints(rect))
+
+        cv.drawContours(mask, [rect], 0, 0, -1)
+    
+    return mask
 
 matplotlib.use('TkAgg')
 
@@ -47,31 +96,18 @@ img_path = f'{img_name}.jpg'
 border = 20
 
 img = cv.imread(img_path)
-#template = cv.imread('template.jpg')
-#template = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
 
 h, w = img.shape[:2]
 
 img = cv.copyMakeBorder(img, border, border, border, border, cv.BORDER_CONSTANT, None, (255, 255, 255))
-#plt.imshow(img[..., ::-1])
-#plt.show()
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-plt.imshow(hsv)
-plt.show()
 
 h,s,v = cv.split(hsv)
-"""
-plt.imshow(s)
-plt.show()
-plt.imshow(v)
-plt.show()
-"""
 #plt.imshow(hsv)
 #plt.show()
 black_thr = [(0, 0, 0), (255, 255, 100)]
 white_thr = [(0, 0, 120), (255, 255, 255)]
 thr = cv.inRange(hsv, black_thr[0], black_thr[1])
-
 
 ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (20, 20))
 thr = cv.morphologyEx(thr, cv.MORPH_CLOSE, ker)
@@ -97,8 +133,9 @@ if len(approx) != 4:
     print('Black board not found')
 
 points = [ point[0] for point in approx ]
+points = order_points(np.array(points))
 
-top_left, bottom_left, bottom_right, top_right = points[:4]
+top_left, top_right, bottom_right, bottom_left = points[:4]
 
 x, y = 0, 0
 h, w = 0, 0
@@ -109,8 +146,6 @@ y1 = top_left[1] if top_left[1] < top_right[1] else top_right[1]
 x2 = top_right[0] if top_right[0] < bottom_right[0] else bottom_right[0]
 y2 = bottom_left[1] if bottom_left[1] < bottom_right[1] else bottom_right[1]
 
-
-
 cropped = gray[y1:y2, x1:x2]
 h, w = cropped.shape[:2]
 
@@ -120,83 +155,53 @@ offset_x = w//4
 cropped = cropped[:h-offset_y, ...]
 cropped = cropped[:, :w-offset_x]
 cropped = cropped[:, offset_x:]
-cropped_hsv = hsv[y1:y2, x1:x2]
 
-#cropped[h-offset_y:, ...] = 0
-#cropped[:, :offset_x] = 0
-#cropped[:, w-offset_x:] = 0
-#cropped_hsv[h-offset_y:, ...] = [0, 0, 0]
-
-#blur = cv.boxFilter(cropped, -1, (31,31))
-
+cropped_bgr = img[y1:y2, x1:x2]
+cropped_bgr = cropped_bgr[:h-offset_y, ...]
+cropped_bgr = cropped_bgr[:, :w-offset_x]
+cropped_bgr = cropped_bgr[:, offset_x:]
 
 
 blur = cv.GaussianBlur(cropped, (31, 31), 60)
-plt.imshow(blur, cmap='gray')
-plt.show()
+#plt.imshow(blur, cmap='gray')
+#plt.show()
 
-_, thr = cv.threshold(blur, 75, 255, cv.THRESH_BINARY)
+_, mask = cv.threshold(blur, 75, 255, cv.THRESH_BINARY)
+ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (25, 25))
+mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, ker)
 
-plt.imshow(thr, cmap='gray')
-plt.show()
+masked = cv.bitwise_and(cropped_bgr, cropped_bgr, mask=mask)
 
-thr = cv.bitwise_and(cropped, cropped, mask=thr)
-canny = cv.Canny(thr, 30, 80, 5)
-ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (21, 21))
-#canny = cv.morphologyEx(canny, cv.MORPH_CLOSE, ker)
-canny = cv.dilate(canny, ker)
-plt.imshow(canny, cmap='gray')
+mask = remove_lateral_white_bands(masked)
+
+print(masked.shape)
+print(mask.shape)
+
+masked = cv.bitwise_and(masked, masked, mask=mask)
+plt.imshow(masked)
 plt.show()
 
 """
-# ----------- White lateral band remove -----------------
-thr = cv.bitwise_and(cropped_hsv, cropped_hsv, mask=thr)
-plt.imshow(thr)
+# ----- Morphological gradient -------
+gradient_ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3,3))
+gradient = cv.morphologyEx(mask, cv.MORPH_GRADIENT, gradient_ker)
+plt.imshow(gradient, cmap='gray')
 plt.show()
-
-thr = cv.inRange(thr, (19, 15, 180), (24, 65, 230))
-ker = cv.getStructuringElement(cv.MORPH_ELLIPSE, (20, 20))
-thr = cv.morphologyEx(thr, cv.MORPH_CLOSE, ker)
-thr = cv.morphologyEx(thr, cv.MORPH_OPEN, ker)
-plt.imshow(thr, cmap='gray')
-plt.show()
-
-contours = cv.cvtColor(cropped, cv.COLOR_GRAY2BGR)
-
-cnts, hierarchy = cv.findContours(thr, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-tmp_cnt = []
-for i, cnt in enumerate(hierarchy[0]):
-    #if cnt[3] == -1:
-    perimeter = cv.arcLength(cnts[i], True)
-    tmp_cnt.append([i, perimeter])
-    epsilon = 0.01 * perimeter
-    approx = cv.approxPolyDP(cnts[i], epsilon, True)
-
-    if len(approx) != 4:
-        continue
-    
-    cv.drawContours(contours, [approx], 0, (0, 255, 0), 3)
-    points = [ point[0] for point in approx ]
-    
-    top_left, bottom_left, bottom_right, top_right = points[:4]
-        
-#cv.drawContours(contours, cnts, -1, (0,0,255), 3)
-plt.imshow(contours)
-plt.show()
-
-# ------------------------------------------------------
+# -------------------------------------
 """
 
 """
-dst = cv.Canny(thr, 100, 255)
+cropped = cv.pyrDown(cropped)
+cropped = cv.pyrUp(cropped)
+
+dst = cv.Canny(cropped, 90, 160)
 plt.imshow(dst, cmap='gray')
 plt.show()
 
 cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
-cdstP = np.copy(cdst)
+#cdst = masked
 
-lines = cv.HoughLines(dst, 1, np.pi / 180, 800, None, 0, 0)
-
+lines = cv.HoughLines(dst, 1, np.pi / 180, 200, None, 0, 0)
 if lines is not None:
     for i in range(0, len(lines)):
         rho = lines[i][0][0]
